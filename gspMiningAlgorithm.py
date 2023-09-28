24e1ce6c5c2f322da3e90d4bab2515349ee1e62a
Dheerajkumarbuchala <buchaladheerajkumar@gmail.com>
2023-09-28 01:06:29 -0500

    comments added and changed regex patterns for efficiency

--- gspMiningAlgorithm.py
+++ gspMiningAlgorithm.py
@@ -9,28 +9,41 @@ def read_data_file(data_file):
     return sequences
 
 def read_parameter_file(parameter_file):
+    # Dictionary to store items and respective MIS values
     mis_values_dict = {}
+    # Variable to store SDC value for other items
     sdc = 0
+
     file = open(parameter_file, 'r')
+
     for i in file:
+        # Code to extract the MIS values of the items
         if i.startswith('MIS'):
-            pattern = r'[-+]?\d*\.\d+|\d+'
-            res = re.findall(pattern, i)
+            # Regex pattern to extract the numbers from the String
+            pattern = r'[-+]?\d*\.\d+|\d+' 
+            res = re.findall(pattern, i) # res -> is a list of two numbers
+            # res[0] = item
+            # res[1] = value
             mis_values_dict[int(res[0])] = float(res[1])
+        # Code to extract the SDC value form the file
         elif i.startswith('SDC'):
+            # Regex pattern to extract the SDC value from the String
             pattern = "\d+\.\d+"
-            sdc = re.findall(pattern, i)
+            sdc = re.findall(pattern, i) # sdc -> is a list of string type elements
             print("SDC before transformation : ", sdc)
+            # Transforming the type string to float
             for j in range(len(sdc)):
                 sdc[j] = float(sdc[j])
+            
             print("SDC after transformation : ", sdc)
+            
     return mis_values_dict, sdc
 
 
 def data_extraction(data_file, parameter_file, output_file):
     # read the raw data provided in the file and transform into usable data
     data = read_data_file(data_file)
-    #print(data)
+    # print(data)
     data_mis , sdc_value = read_parameter_file(parameter_file)
     print("MIS Values : ", data_mis)
     print("SDC Value : ", sdc_value)
